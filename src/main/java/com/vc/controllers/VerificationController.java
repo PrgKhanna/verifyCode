6f062ed35a28c9bdeba04a3e32e01fed075a39c8
Parag Khanna <parag.khanna@agoda.com>
2018-06-12 21:34:49 +0700

    fix apis

--- src/main/java/com/vc/controllers/VerificationController.java
+++ src/main/java/com/vc/controllers/VerificationController.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,22 +28,26 @@ public class VerificationController {
 	@Autowired
 	private VerificationCodeResponseGeneratorService verificationCodeResponseGeneratorService;
 
-	@RequestMapping(value = "/verify/{userId}", method = RequestMethod.POST, produces = {
-			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
-	public ResponseEntity<GenerateVerificationCodeResponseDTO> generator(@PathVariable("userId") int userId) {
-		LOGGER.info("Generating Verification Code for User : " + userId);
-		GenerateVerificationCodeResponseDTO response = verificationCodeResponseGeneratorService
-				.getGenerateVerificationCode(userId);
-		return new ResponseEntity<GenerateVerificationCodeResponseDTO>(response, HttpStatus.OK);
-	}
+	// @RequestMapping(value = "/{userId}", method = RequestMethod.POST, produces =
+	// {
+	// MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	// public ResponseEntity<GenerateVerificationCodeResponseDTO>
+	// generator(@PathVariable("userId") int userId) {
+	// LOGGER.info("Generating Verification Code for User : " + userId);
+	// GenerateVerificationCodeResponseDTO response =
+	// verificationCodeResponseGeneratorService
+	// .getGenerateVerificationCode(userId);
+	// return new ResponseEntity<GenerateVerificationCodeResponseDTO>(response,
+	// HttpStatus.OK);
+	// }
 
 	@RequestMapping(value = "/{userIdWithFormat:.+}", method = RequestMethod.POST)
-	public ResponseEntity<String> generatorAsCSV(
+	public ResponseEntity<String> generatorWithCSV(
 			@Valid @NotNull @PathVariable("userIdWithFormat") String userWithFormat) {
 		String[] params = userWithFormat.split("\\.");
 		Integer userId = params.length > 0 ? Integer.parseInt(params[0]) : -1;
 		String format = params.length > 1 ? params[1] : "json";
-		LOGGER.info("*********  Generating Verification Code for User : " + userId + " and in format : " + format);
+		LOGGER.info("Generating Verification Code for User : " + userId + " and in format : " + format);
 		GenerateVerificationCodeResponseDTO responseDTO = verificationCodeResponseGeneratorService
 				.getGenerateVerificationCode(userId);
 		ParserFactory<GenerateVerificationCodeResponseDTO> parserFactory = new ParserFactory<GenerateVerificationCodeResponseDTO>();
@@ -52,14 +55,34 @@ public class VerificationController {
 		return new ResponseEntity<String>(parser.parse(responseDTO), HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/{userId}/{code}", method = RequestMethod.GET, produces = {
-			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE, "text/csv" })
-	public ResponseEntity<ValidationCodeResponseDTO> validator(@PathVariable("userId") int userId,
-			@PathVariable("code") String code) {
-		LOGGER.info("Validating Verification Code : " + code + " for User : " + userId);
-		ValidationCodeResponseDTO response = verificationCodeResponseGeneratorService.validatorResponse(userId, code);
+	// @RequestMapping(value = "/{userId}/{code}", method = RequestMethod.GET,
+	// produces = {
+	// MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE, "text/csv"
+	// })
+	// public ResponseEntity<ValidationCodeResponseDTO>
+	// validator(@PathVariable("userId") int userId,
+	// @PathVariable("code") String code) {
+	// LOGGER.info("Validating Verification Code : " + code + " for User : " +
+	// userId);
+	// ValidationCodeResponseDTO response =
+	// verificationCodeResponseGeneratorService.validatorResponse(userId, code);
+	//
+	// return new ResponseEntity<ValidationCodeResponseDTO>(response,
+	// HttpStatus.OK);
+	// }
 
-		return new ResponseEntity<ValidationCodeResponseDTO>(response, HttpStatus.OK);
+	@RequestMapping(value = "/{userId}/{codeWithFormat:.+}", method = RequestMethod.GET)
+	public ResponseEntity<String> validator(@PathVariable("userId") int userId,
+			@PathVariable("codeWithFormat") String codeWithFormat) {
+		String[] params = codeWithFormat.split("\\.");
+		String code = params.length > 0 ? params[0] : "";
+		String format = params.length > 1 ? params[1] : "json";
+		LOGGER.info("Validating Verification Code : " + code + " , User : " + userId + " for format : " + format);
+		ValidationCodeResponseDTO responseDTO = verificationCodeResponseGeneratorService.validatorResponse(userId,
+				code);
+		ParserFactory<ValidationCodeResponseDTO> parserFactory = new ParserFactory<ValidationCodeResponseDTO>();
+		Parser<ValidationCodeResponseDTO> parser = parserFactory.getParser(format);
+		return new ResponseEntity<String>(parser.parse(responseDTO), HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/vc/parsers/CsvParser.java
+++ src/main/java/com/vc/parsers/CsvParser.java
@@ -1,5 +1,9 @@
 package com.vc.parsers;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,8 +15,34 @@ public class CsvParser<T> extends Parser<T> {
 	public String parse(T t) {
 		LOGGER.info("Converting to CSV");
 		try {
+			// For this i will use reflection , in case of complex Responses we can use
+			// CsvParser Libraries
+			// Plus this will have performance impact
+			LOGGER.info("Converting to CSV");
+			try {
+				List<String> values = new ArrayList<String>();
+				Class<?> cls = t.getClass();
+				Field[] fields = cls.getDeclaredFields();
+				for (int i = 1; i < fields.length; i++) {
+					Field f = fields[i];
+					boolean isAccesible = f.isAccessible();
 
-			return "";
+					if (!isAccesible) {
+						f.setAccessible(true);
+					}
+
+					values.add(f.get(t).toString());
+
+					if (!isAccesible) {
+						f.setAccessible(isAccesible);
+					}
+
+				}
+				return String.join(", ", values);
+			} catch (Exception e) {
+				// LOGGER.error("Failed to parse to Csv", e.getMessage());
+			}
+			return null;
 		} catch (Exception e) {
 			LOGGER.error("Failed to parse to Csv", e.getMessage());
 		}
